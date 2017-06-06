ac8064bc1e5443ff703b04e9d4d14783f9015c80
Agustin Peñalver Alvaro <ssromancr84@gmail.com>
2017-06-06 13:32:13 +0200

    Programados Aleatorios, con y sin repeticion

--- app/src/main/java/com/example/ssroman/appleatorios/AleatorioConRepeticion.java
+++ app/src/main/java/com/example/ssroman/appleatorios/AleatorioConRepeticion.java
@@ -2,6 +2,12 @@ package com.example.ssroman.appleatorios;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class AleatorioConRepeticion extends AppCompatActivity {
 
@@ -12,4 +18,33 @@ public class AleatorioConRepeticion extends AppCompatActivity {
 
     }
 
+    int n1, n2, rep;
+    String serie;
+
+    public void generarAleatorio1(View v){
+        EditText inicial1  = (EditText)findViewById(R.id.inicial1);
+        EditText final1 = (EditText)findViewById(R.id.final1);
+        EditText repeticiones1 = (EditText)findViewById(R.id.repeticiones1);
+        TextView resultado1 = (TextView)findViewById(R.id.resultado1);
+        resultado1.setText("");
+        n1 = Integer.parseInt(inicial1.getText().toString());
+        n2 = Integer.parseInt(final1.getText().toString());
+        rep = Integer.parseInt(repeticiones1.getText().toString());
+
+            if ((rep!=0)&&(n2>n1)){
+                int limite = 1;
+                Random r = new Random();
+                do {
+                    int dato = r.nextInt(n2-n1) + n1;
+                    serie = String.valueOf(dato);
+                    resultado1.setText(serie + ", " + resultado1.getText());
+                    limite++;
+                } while (limite <= rep); //Numero de veces que genera un aleatorio con el contador limite
+            }else{
+            Toast.makeText(this, "Asigna valor correcto a los operadores", Toast.LENGTH_LONG).show();
+            inicial1.setText("0");
+            final1.setText("0");
+            repeticiones1.setText("0");
+            }
+    }
 }
--- app/src/main/java/com/example/ssroman/appleatorios/AleatorioSinRepeticion.java
+++ app/src/main/java/com/example/ssroman/appleatorios/AleatorioSinRepeticion.java
@@ -2,6 +2,12 @@ package com.example.ssroman.appleatorios;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class AleatorioSinRepeticion extends AppCompatActivity {
 
@@ -11,5 +17,52 @@ public class AleatorioSinRepeticion extends AppCompatActivity {
         setContentView(R.layout.activity_aleatorio_sin_repeticion);
 
     }
+    int n1, n2, rep, comprobar;
+    String serie;
 
-}
+
+    public void generarAleatorio2(View v) {
+        EditText inicial2 = (EditText) findViewById(R.id.inicial2);
+        EditText final2 = (EditText) findViewById(R.id.final2);
+        EditText repeticiones2 = (EditText) findViewById(R.id.repeticiones2);
+        TextView resultado2 = (TextView) findViewById(R.id.resultado2);
+        int i, j, k, rango;
+        resultado2.setText("");
+        n1 = Integer.parseInt(inicial2.getText().toString());
+        n2 = Integer.parseInt(final2.getText().toString());
+        rep = Integer.parseInt(repeticiones2.getText().toString());
+        rango = n2-n1;
+        int [] numeros = new int [rep];
+        if ((rep < rango) && (rep != 0) && (n2 > n1)) {
+            numeros[0]=aleatorio(n1, n2);
+            for (i = 1; i < rep; i++) {
+                numeros[i]=aleatorio(n1, n2);
+                for (j=0; j<i; j++) {
+                     if (numeros[i] == numeros[j]) {
+                        i--;
+                    }// fin if
+                }// fin del jor j
+            } // fin del for i
+
+            for (k=0; k < rep; k++){
+                serie = String.valueOf(numeros[k]);
+                resultado2.setText(serie + ", " + resultado2.getText());
+            }
+
+            } else {
+            Toast.makeText(this, "Asigna valor correcto a los operadores", Toast.LENGTH_LONG).show();
+            inicial2.setText("0");
+            final2.setText("0");
+            repeticiones2.setText("0");
+        }
+    }
+
+    public int aleatorio (int n1, int n2){
+
+        Random r = new Random();
+            int dato = r.nextInt(n2-n1) + n1;
+            return dato;
+         //Numero de veces que genera un aleatorio con el contador limite
+    }
+    }
+
